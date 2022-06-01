be3d9d42c4000419688384c187daebf808144650
John Philip Calugcugan <jcalugcugan@webcreek.com>
2022-06-01 20:52:57 +0800

    Initial Commit

---
+++ DigitalRoot/DigitalRoot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DigitalRoot
+{
+    public class DigitalRootHelper
+    {
+        public int DigitalRoot(long n)
+        {
+            return DigitalRootCreator(n);
+        }
+
+        private int DigitalRootCreator(long input)
+        {
+            int output = 0;
+            bool isCompleted = false;
+            string inputInStr = input.ToString();
+
+            while (!isCompleted)
+            {
+                var inputArr = inputInStr.ToCharArray();
+                inputInStr = GetTotalOfInputArr(inputArr).ToString();
+
+                if (inputInStr.ToCharArray().Length == 1)
+                {
+                    isCompleted = true;
+                    output = System.Convert.ToInt32(inputInStr);
+                }
+            }
+            return output;
+        }
+
+        private int GetTotalOfInputArr(char[] inputArr)
+        {
+            var output = 0;
+
+            foreach (var single in inputArr)
+            {
+                var wkInt = 0;
+                var isConverted = int.TryParse(single.ToString(), out wkInt);
+
+                if (isConverted)
+                {
+                    output += wkInt;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Digital root is the recursive sum of all the digits in a number.
+                Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+            
+                Example:
+                    16  -->  1 + 6 = 7
+                    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+                    132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+                    493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+            */
+
+            var digitalRootHelper = new DigitalRootHelper();
+            Console.WriteLine(digitalRootHelper.DigitalRoot(16)); // Answer 1
+            Console.WriteLine(digitalRootHelper.DigitalRoot(942)); // Answer 2
+            Console.WriteLine(digitalRootHelper.DigitalRoot(132189)); // Answer 3
+            Console.WriteLine(digitalRootHelper.DigitalRoot(493193)); // Answer 4
+
+            Console.ReadLine(); // Wait for press key before exit
+        }
+    }
+}
